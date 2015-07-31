4363256bc75ecd2c660c12d9e663fcdb559a11fc
ahmar siddiqui <ahmar.siddiqui@gmail.com>
2015-08-01 05:23:34 +0530

    reduced api calls sorted according to timeone affinity and some weird stuff

--- app.js
+++ app.js
@@ -1,15 +1,53 @@
 (function(window,undefined)
 {
 	'use strict';
+	var total;
+	var people=[];
 	$(function(){
 		$.get('people.json',function(resp)
 		{
 			var json=(typeof resp !=='object')?JSON.parse(resp):resp;
-			$("#container").html(fetchDateTime(json));
+			total=json.length;
+			fetchDateTime(json);
 		});
 	});
+	var fetchDateTime=function(obj)
+	{
+		var html=[];
+		var h=[];
+		obj.forEach(function(a,b)
+		{
+			
+			fetchTimeZone(a.city,function(city,timeObj)
+			{
+
+				a.tz=moment().tz(timeObj.timeZoneId).format('MM-DD-YYYY hh:mm');
+				a.timezone=timeObj.timeZoneId;
+				a.zone=moment().tz(timeObj.timeZoneId).format('z');
+				$.get('https://api.github.com/users/'+a.ghuser+'?access_token=e67ea19adddc71d25778fc28344e10e53c04b22f',function(resp)
+				{
+					a.name=resp.name||a.name;
+					total--;
+					a.image=resp.avatar_url;
+					a.url=resp.html_url;
+					a.timeObj=timeObj;
+					people.push(a);
+					if(total<=1)
+						$("#container").html(print());
+				}).fail(function()
+				{
+					total--;
+				});
+			});
+		});
+	};
 	var fetchTimeZone=function(city,callback)
 	{
+		if(localStorage[city]){
+			var t=JSON.parse(localStorage[city]);
+			callback(city,t);
+			return;
+		}
 		var url="http://maps.googleapis.com/maps/api/geocode/json?address=";
 		$.get(url+city,function(resp)
 		{
@@ -21,39 +59,40 @@
 			var url='https://maps.googleapis.com/maps/api/timezone/json?location='+latLng+'&timestamp=1331766000&key=AIzaSyCh78qhRtWIEtKdUkWm1uOHZUzSVgbClaI';
 			$.get(url,function(resp)
 			{
+				// console.log(resp);
+				localStorage[city]=JSON.stringify(resp);
 				if(resp.timeZoneId)
 				{
-					callback(city,resp.timeZoneId);
+					callback(city,resp);
 				}
 			});
 		});
 	};
-	var fetchDateTime=function(obj)
-	{
-		var html=[];
-		var h=[];
-		obj.forEach(function(a,b)
-		{
-			fetchTimeZone(a.city,function(city,timeZone)
-			{
-				a.tz=moment().tz(timeZone).format('MM-DD-YYYY hh:mm');
-				a.timezone=timeZone;
-				$.get('https://api.github.com/users/'+a.ghuser+'?access_token=e67ea19adddc71d25778fc28344e10e53c04b22f',function(resp)
-				{
-					a.name=resp.name||a.name;
-					a.image=resp.avatar_url;
-					a.url=resp.html_url;
-					$("#container").append(print([a]));
-				});
-			});
-		});
-	};
-
-var print=function(obj)
+var sort=function()
+{
+ var sorted=[];
+ console.log('sorting started');
+ people=people.sort(function(a,b)
+ {
+ 	/*console.log(a.timeObj.rawOffset+' a');
+ 	console.log(b.timeObj.rawOffset+' b');*/
+ 	if(a.timeObj.rawOffset<b.timeObj.rawOffset)
+ 		return -1;
+ 	else if(a.timeObj.rawOffset>b.timeObj.rawOffset)
+ 		return 1;
+ 	return 0;
+ });
+};
+var print=function()
 	{
 		
 		var html=[];
-		obj.forEach(function(a,b)
+		console.log('before sort');
+		console.log(people);
+		sort();
+		console.log('after sort');
+		console.log(people);
+		people.forEach(function(a,b)
 		{
 			// console.log(a);
 			var h=([
@@ -71,7 +110,9 @@ var print=function(obj)
 						a.name,
 						"</span>",
 					'</div>',
-					'<div></div>',
+					'<div>',
+						a.zone,
+					'</div>',
 					'<div class="timezone" data-locale='+a.timezone+'>',
 						a.tz,
 					'</div>',
@@ -92,5 +133,11 @@ var print=function(obj)
 			th.html(newTime);
 		});
 	},60000);
+
+
+
+})(window);
+(function(window,undefined)
+{
 
 })(window);
